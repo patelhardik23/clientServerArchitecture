15d2ec345df08e7fa4819028ff1073c6083214f1
Hardik Patel <hardik.p@ezdi.us>
2020-04-07 20:05:49 +0530

    UI configuration

--- boinc-volunteer-computing-worker/src/Worker/Worker.java
+++ boinc-volunteer-computing-worker/src/Worker/Worker.java
@@ -1,21 +1,10 @@
 package Worker;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Worker {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        int objectPort = 6789;
-        int filePort = 6790;
-        try {
-            Client client = new Client("localhost", objectPort, filePort);
-            client.test();
-            client.closeConnection();
-        } catch (IOException ex) {
-            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+        WorkerUI workerUi = new WorkerUI();
+        workerUi.setVisible(true);
     }
 }
